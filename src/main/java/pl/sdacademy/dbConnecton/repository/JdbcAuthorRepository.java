1540f890b1b59447f63a6817821446b127b5bd3a
Pawel Pluta <pawelplutakatowice@gmail.com>
2018-12-15 10:26:49 +0100

    FInding author by name and lastname

---
+++ src/main/java/pl/sdacademy/dbConnecton/repository/JdbcAuthorRepository.java
@@ -0,0 +1,51 @@
+package pl.sdacademy.dbConnecton.repository;
+
+import pl.sdacademy.dbConnecton.model.Author;
+import pl.sdacademy.dbConnecton.service.repository.AuthorRepository;
+
+import java.sql.*;
+import java.util.Optional;
+
+public class JdbcAuthorRepository implements AuthorRepository {
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/library?serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true";
+    private static final String DB_USER = "developer";
+    private static final String DB_PASSWORD = "developer";
+
+    @Override
+    public Optional<Author> findByFirstAndLastName(String firstName, String lastName) {
+        Author foundAuthor = null;
+        try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT id AS mojeId, firstName, lastName " +
+                    "FROM author " +
+                    "WHERE firstName = '" + firstName + "' " +
+                    "AND lastName = '" + lastName + "'");
+            if (rs.next()) {
+                Long authorId = rs.getLong("mojeId");
+                String authorName = rs.getString("firstName");
+                String authorLastName = rs.getString(3);
+                foundAuthor = new Author(authorId, authorName, authorLastName);
+            }
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Optional.ofNullable(foundAuthor);
+    }
+
+    @Override
+    public Optional<Author> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Author save(Author entity) {
+        return null;
+    }
+
+    @Override
+    public Author update(Author entity) {
+        return null;
+    }
+}
